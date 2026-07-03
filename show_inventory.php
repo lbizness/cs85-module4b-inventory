14411d35e5932fb26a2f3266ca8b4669996a9426
Luke <lukebisgard@gmail.com>
2026-07-03 08:58:17 -0700

    Set up HTML for showing inventory

--- show_inventory.php
+++ show_inventory.php
@@ -1,16 +1,16 @@
-CREATE DATABASE inventory_db;
-USE inventory_db;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Show Inventory</title>
+</head>
+<body>
+<?php
+//Luke Bisgard
+//GITHUB LINK: https://github.com/lbizness/cs85-module4b-inventory
+//PHP Code Here
 
-CREATE TABLE items (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  item_name VARCHAR(100) NOT NULL,
-  category VARCHAR(50),
-  quantity INT,
-  purchase_date DATE
-);
-  
-INSERT INTO items (item_name, category, quantity, purchase_date)
-VALUES
-('LED Desk Lamp', 'Electronics', 2, '2024-06-01'),
-('Notebook', 'Stationery', 10, '2024-05-15');
-  
+
+//GITHUB LINK: https://github.com/lbizness/cs85-module4b-inventory
+?>
+</body>
